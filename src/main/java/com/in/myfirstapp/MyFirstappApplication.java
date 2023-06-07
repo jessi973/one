1650c8875d7bd63000ca795719b928abb6918219
DivajiBannu <jessijessi973@gmail.com>
2023-06-07 12:04:21 +0530

    first commit

---
+++ src/main/java/com/in/myfirstapp/MyFirstappApplication.java
@@ -0,0 +1,13 @@
+package com.in.myfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in/myfirstapp/MyFirstappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.myfirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
